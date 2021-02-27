e865ff6658814b5a658c2065ec72701cb99f2f68
Rohith R <rohithrajagopal51@gmail.com>
2021-02-27 17:00:55 +0530

    tcp_server code has been added and tcp _client code has been change to establish connection with the tcp_server

--- Network basics/tcp_client.py
+++ Network basics/tcp_client.py
@@ -1,16 +1,16 @@
 import socket
 
-target_host = "www.google.com" #the host we need to send or test with our tcp client
-target_port = 80 # port
+target_host = "127.0.0.1" #the host we need to send or test with our tcp client
+target_port = 9999 # port
 # client object creation
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connection establishment
 client.connect((target_host,target_port))
 #data we need to send to the host
-client.send("GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n".encode())
+client.send("hello server".encode())
  #response from client and save
 
-response = client.recv() #4096 is the size of the recieve data
+response = client.recv(1024) #4096 is the size of the recieve data
 print(type(response))
 print(response)
